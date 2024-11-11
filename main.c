08d780b6fd8193464c2969b9902e95948cd54af9
André Ferreira <andreelzs@hotmail.com>
2024-11-11 11:08:01 -0300

    a

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int opcaoMenu;
+    
+    printf("Bem vindo ao sistema da (nomedaloja)");
+    printf("\nDigite a opção do menu que você precisa");
+    scanf("%d", &opcaoMenu);
+    
+    while(opcaoMenu =! 4);
+    switch(opcaoMenu){
+                      case '1'{
+                           printf("Cadastrar novas vendas");    
+                      }
+                      case '2'
     return 0;
 }
+
